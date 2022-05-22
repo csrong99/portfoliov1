fd09e884cc299fe4b29076dd147482f8e9e9dcd5
Chuah Seong Rong <chuahsr99@gmail.com>
2022-05-22 19:49:59 +0800

    enhances some design and added some new function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// ===================== MOBILE TOGGLE MENU =====================
+
 const navMenu = document.getElementById("nav-menu")
 const navToggle = document.getElementById("nav-toggle")
 const navClose = document.getElementById("nav-close")
@@ -14,6 +16,9 @@ if(navClose) {
         navMenu.classList.remove("show-menu");
     })
 }
+
+
+// ===================== TOGGLE THEME =====================
 
 const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark';
@@ -34,7 +39,7 @@ if(selectedTheme){
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-//activate/deactivate theme manually w button
+// ===================== activate/deactivate theme manually w button =====================
 themeButton.addEventListener('click',()=>{
     //add/remove dark/icon theme
     document.documentElement.classList.toggle(darkTheme)
@@ -45,6 +50,61 @@ themeButton.addEventListener('click',()=>{
     localStorage.setItem('selected-icon',getCurrentIcon())
 });
 
-//scrolling number 
+// ===================== SROLL SECTIONS ACTIVE LINK =====================
+
+const sections = document.querySelectorAll('section[id]');
+console.log(sections);
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        // console.log('sectionId = ' + sectionId);
+        // console.log('sectionHeight = ' + sectionHeight);
+        // console.log('sectionTop = ' + sectionTop);
 
 
+        if(scrollY > sectionTop && scrollY <=  sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+// ===================== SROLL SECTIONS ACTIVE LINK =====================
+
+function scrollHeader() {
+    const nav = document.getElementById("header");
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+// ===================== ACCORDIAN SKILLS =====================
+
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__headers');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skill__content skills__close'
+    }
+
+    if(itemClass == 'skill__content skills__close') {
+        this.parentNode.className = 'skill__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
